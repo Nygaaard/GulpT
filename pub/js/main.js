a2adf9ffc66f6fd0a45c29c1e31917c5c3cc6f88
Andreas Nygård <anny2301@student.miun.se>
2024-01-17 13:31:07 +0100

    Added background color

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,1 @@
-"use strict";console.log("Hello from one.js"),console.log("Hello from two.js");
+"use strict";let openEl=document.getElementById("open"),closeEl=document.getElementById("close"),hiddenEl=document.getElementById("hidden");openEl.addEventListener("click",(function(){hiddenEl.style.display="flex"})),closeEl.addEventListener("click",(function(){hiddenEl.style.display="none"})),console.log("Hello from two.js");
