fc88bc1f1acfb0e77503f5076e1f07ff1c8b4fbc
Andreas Nygård <anny2301@student.miun.se>
2024-01-17 13:28:22 +0100

    Added buttons and div

--- src/js/one.js
+++ src/js/one.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log("Hello from one.js");
+let openEl = document.getElementById("open");
+let closeEl = document.getElementById("close");
+let hiddenEl = document.getElementById("hidden");
+
+openEl.addEventListener("click", function(){
+    hiddenEl.style.display = "flex";
+})
+
+closeEl.addEventListener("click", function(){
+    hiddenEl.style.display = "none";
+})
+
